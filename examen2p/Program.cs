5b8ed55e79c127bb8f002190136bfe9ce4b4e45d
Lab-Informatica10 <Lab-Informatica10@DESKTOP-F6EFNQC>
2024-07-12 16:15:28 -0400

    Agregar archivos de proyecto.

---
+++ examen2p/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class MaterialBibliografico
+{
+
+    public string Titulo;
+    public string Autor;
+
+    public MaterialBibliografico(string titulo, string autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+
+    }
+
+    public static void MostrarInformacion()
+    {
+        Console.WriteLine(($"Título: {Titulo}, Autor: {Autor}"));
+    }
+}
+
+//Aqui derivo
+
+public class Libro : MaterialBibliografico
+{
+    public int NumeroDePaginas;
+
+    public Libro (string titulo, string autor, string numeroDePaginas)
+        : base(titulo, autor);
+
+        public void MostrarInformacion();
+
+}
+
+public class Revista : MaterialBibliografico
+{
+    public int NumeroDePaginas;
+
+    public Libro(string titulo, string autor, string numeroDePaginas,string revista)
+        : base(titulo, autor)
+
+        public void MostrarInformacion();
+
